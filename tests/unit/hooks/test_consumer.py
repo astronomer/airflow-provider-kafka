e119918ea89a9ac02299c15a6567a00d9243c2d6
Dylan Storey <dstorey@Dylans-MacBook-Pro.local>
2022-02-05 15:14:56 -0500

    consumer in

---
+++ tests/unit/hooks/test_consumer.py
@@ -0,0 +1,59 @@
+"""
+Unittest module to test Hooks.
+
+Requires the unittest, pytest, and requests-mock Python libraries.
+
+Run test:
+
+    python3 -m unittest tests.hooks.test_sample_hook.TestSampleHook
+
+"""
+
+from ast import excepthandler
+import logging
+import os
+from airflow import AirflowException
+import pytest
+import unittest
+from unittest import mock
+
+
+#
+from confluent_kafka import Consumer
+
+# Import Hook
+from kafka_provider.hooks.consumer import ConsumerHook
+
+
+log = logging.getLogger(__name__)
+
+
+# Mock the `conn_sample` Airflow connection
+@mock.patch.dict('os.environ', AIRFLOW_CONN_KAFKA_SAMPLE='localhost:9092')
+class TestConsumerHook(unittest.TestCase):
+    """
+    Test consumer hook.
+    """
+
+    def test_init(self):
+        ''' test initialization of AdminClientHook'''
+
+        #Standard Init
+        extra_configs = {'socket.timeout.ms': 10}
+        c = ConsumerHook(['test_1'], kafka_conn_id='kafka_samle', config=extra_configs)
+        
+        #Too Many Args
+        with pytest.raises(AirflowException):
+            extra_configs = {'bootstrap.servers': 'localhost:9092'}
+            c = ConsumerHook(['test_1'], kafka_conn_id='kafka_samle', config=extra_configs)
+            
+        #Not Enough Args
+        with pytest.raises(AirflowException):
+            extra_configs = {}
+            c = ConsumerHook(['test_1'], config=extra_configs)
+            
+
+
+
+if __name__ == '__main__':
+    unittest.main()
