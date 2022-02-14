2a196663e8ee83cc2b2d06de52194a48bd96dd63
Dylan Storey <dylan.storey@astronomer.io>
2022-02-14 16:47:06 -0500

    await message started

---
+++ kafka_provider/triggers/await_message.py
@@ -0,0 +1,74 @@
+import asyncio
+from functools import partial
+from asyncore import poll
+from os import sync
+from typing import Any, Dict, Optional, Tuple, Union, Callable
+
+from airflow import AirflowException
+from airflow.triggers.base import BaseTrigger, TriggerEvent
+
+from asgiref.sync import async_to_sync
+from kafka_provider.hooks.consumer import ConsumerHook
+from kafka_provider.shared_utils import get_callable
+
+
+
+class AwaitMessage(BaseTrigger):
+
+    def __init__(
+        self,
+        topics: Sequence[str],
+        apply_function: str,
+        apply_function_args: Sequence[Any],
+        apply_function_kwargs: Dict[Any,Any],
+        kafka_conn_id: Optional[str] = None,
+        kafka_config: Optional[Dict[Any,Any]] = None,
+        poll_timeout: float = 1,
+    )-> None:
+
+        self.topics = topics
+        self.apply_function = apply_function
+        self.apply_function_args = apply_function_args
+        self.apply_function_kwargs = apply_function_kwargs
+        self.kafka_conn_id = kafka_conn_id
+        self.kafka_config = kafka_config
+        self.poll_timeout = poll_timeout
+
+
+    def serialize(self) -> Tuple[str, Dict[str, Any]]:
+        return(
+            'kafka_provider.triggers.await_message.AwaitMessage',
+            {
+
+            }
+        )
+
+
+    async def run(self):
+        consumer = ConsumerHook(topics = self.topics, kafka_conn_id=self.kafka_conn_id, config=self.kafka_config, no_broker=self.no_broker)
+        
+        method = sync_to_async(instance.get_consumer)
+        consumer = await method()
+
+        async_poll = sync_to_async(consumer.poll)
+        async_commit = sync_to_async(consumer.commit)
+
+        processing_call = get_callable(self.apply_function)
+        processing_call = partial(processing_call,*self.apply_function_args, **self.apply_function_kwargs)
+        async_message_process = sync_to_async(processing_call)
+        while True:
+
+            message = await async_poll(self.poll_timeout)
+
+            rv = async_message_process(message)
+            if rv:
+                yield TriggerEvent(rv)
+            async_commit(asynchronous=False)
+
+            # get the consumer
+            # await poll return
+            # await message processing
+            # if callable has a value : yield TriggerEvent(return Value)
+
+    
+ 
