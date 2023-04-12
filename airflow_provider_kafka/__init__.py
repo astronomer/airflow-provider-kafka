ebc97ce6b012fbf6bd63e12fd09f70288c4f9a19
Dylan Storey <dylan.storey@gmail.com>
2023-04-11 21:42:46 -0400

    starting deprecation process for the package

--- airflow_provider_kafka/__init__.py
+++ airflow_provider_kafka/__init__.py
@@ -2,8 +2,10 @@
 # for certain features. We recognize it's a bit unclean to define these in
 # multiple places, but at this point it's the only workaround if you'd like your
 # custom conn type to show up in the Airflow UI.
+import sys
+import warnings
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 def get_provider_info():
@@ -13,3 +15,8 @@ def get_provider_info():
         "description": "Airflow hooks and operators for Kafka",  # Required
         "versions": [__version__],  # Required
     }
+
+
+
+if not 'sdist' in sys.argv:
+    warnings.warn("This package has been deprecated and incorporated into the apache airflow project. Please install via `pip install apache-airflow[apache.kafka]`", DeprecationWarning, stacklevel=2)
